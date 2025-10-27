2dbb81f0a61d2b2288499ad67e4ddff448f2969e
Shashikant Shah <shashikantshah951@outlook.com>
2025-10-27 22:51:32 +0530

    commit

---
+++ react-redux/src/components/CakeContainer.jsx
@@ -0,0 +1,10 @@
+function CakeContainer() {
+    return (
+        <div>
+            <h3>Number of cake</h3>
+            <button>Buy cake</button>
+        </div>
+    )
+}
+
+export default CakeContainer;
