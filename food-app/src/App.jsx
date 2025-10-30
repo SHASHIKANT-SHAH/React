762f0285fc2db82d9ff11256cb108afa1f4cdd82
Shashikant Shah <shashikantshah951@outlook.com>
2025-10-30 11:14:29 +0530

    commit

---
+++ food-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React, { Fragment } from 'react'
+import Header from './Components/Layout/Header'
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  )
+}
+
+export default App
